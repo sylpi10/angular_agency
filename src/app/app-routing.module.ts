4c9f87f3c38b6437201e58ea3cd6357291113918
sylpi <syl.pillet@hotmail.fr>
2020-06-16 00:37:15 +0200

    create propertiesService with datas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AdminDashboardComponent} from "./admin/admin-dashboard/admin-dashboard.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomeComponent },
+  { path:'admin/dashboard', component: AdminDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
