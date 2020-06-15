b627556dfd4c3522feadd7d0228d7088057364df
sylpi <syl.pillet@hotmail.fr>
2020-06-15 23:41:40 +0200

    display items on home with fake data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myAgency';
+
+  properties = [
+    {
+      title: 'Amazing house',
+      category: 'House',
+      sold: true
+    },
+    {
+      title: 'Amazing appartment',
+      category: 'Appartment',
+      sold: false
+    },{
+      title: 'Amazing Ranch',
+      category: 'Ranch',
+      sold: true
+    }
+  ];
+
+  getSoldStatus(index){
+    return this.properties[index].sold ? '#ec4e4e' : '#37ac29';
+  }
 }
