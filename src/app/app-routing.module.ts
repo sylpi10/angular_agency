15f1cbc130218e22658ffdbc4df2fa8a700136e2
sylpi <syl.pillet@hotmail.fr>
2020-06-19 15:44:18 +0200

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import {HomeComponent} from "./home/home.component";
 import {AdminDashboardComponent} from "./admin/admin-dashboard/admin-dashboard.component";
 import {SigninComponent} from "./authentication/signin/signin.component";
 import {PropertyDetailComponent} from "./property-detail/property-detail.component";
+import {AuthGuardService} from "./services/auth-guard.service";
 
 
 const routes: Routes = [
   { path:'home', component: HomeComponent },
-  { path:'admin/dashboard', component: AdminDashboardComponent },
+  { path:'admin/dashboard', canActivate: [AuthGuardService], component: AdminDashboardComponent },
   { path:'login', component: SigninComponent },
   { path:'property/:id', component: PropertyDetailComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
